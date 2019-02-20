42e55b1347565880a702358147ddfebe323d16bc
Andrea Slyngstadli <andrsly@stud.ntnu.no>
2019-02-20 13:35:52 +0100

    Added html, js and css

---
+++ app/src/main/layout/js/main.js
@@ -0,0 +1,29 @@
+//Initializing the map
+function setMap() {
+    console.log("Loading map");
+  
+    var map = L.map("map").setView([63.433, 10.395], 15 );
+    //Set view takes two parameters;
+    //1. The coordinates for the center of the map
+    //2. The zoom level. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
+  
+    //Adding the base map. Base map decides how the background map looks like
+    var basemapUrl = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
+    L.tileLayer(basemapUrl).addTo(map);
+  
+    //Adding geoJSON layer to the map:
+    L.geoJSON(utesteder, {
+      onEachFeature: onEachFeature
+    }).addTo(map);
+  }
+  
+  
+  
+  function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.name) {
+      layer.bindPopup("<h4>" + feature.properties.name + "</h4> <p> " + feature.properties.desc + "</p> <img class='test' src =" + feature.properties.image +  "> </img>");
+    };
+  
+  }
+  
+  window.onload = setMap;
