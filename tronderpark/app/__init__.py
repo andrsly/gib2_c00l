b1304943fcebfb1ede310160f4125fac3c9d3a66
Shahitha Sothinathan <shahisothi@dhcp-10-24-14-160.wlan.ntnu.no>
2019-03-12 09:36:32 +0100

    Added gunicorn fro heroku (++)

--- tronderpark/app/__init__.py
+++ tronderpark/app/__init__.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 from app import routes
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(port=80,debug=True)
+   #app.set( 'port', ( process.env.PORT=5000 ))
+   #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
