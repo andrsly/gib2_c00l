3e91b153fe7672d587838370a9a97b944909a54a
Rikke Landmark <rikkebl@stud.ntnu.no>
2019-03-23 13:21:12 +0100

    fixed register

--- tronderpark/app/routes.py
+++ tronderpark/app/routes.py
@@ -38,11 +38,10 @@ def login():
         else:
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
-
+    
 @app.route('/register/')
 def register():
     return render_template('register.html')
-
 
 if __name__ == "__main__":
    app.run(port=80,debug=True)
