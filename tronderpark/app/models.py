e28907002dc1dd9ec748ca698fed24fbd00865e5
Shahitha Sothinathan <shahisothi@dhcp-10-24-12-49.wlan.ntnu.no>
2019-03-29 13:47:12 +0100

    Added autoincrement

--- tronderpark/app/models.py
+++ tronderpark/app/models.py
@@ -6,7 +6,7 @@ from flask_login import UserMixin
 
 class user1(UserMixin, db.Model):
     ___tablename__ = "user1"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
